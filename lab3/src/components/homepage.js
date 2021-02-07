f52963c85481be54afbee7e4292cf76952f97616
ta396618 <ravita@Knights.ucf.edu>
2021-02-07 08:59:34 +0500

    lab3

---
+++ lab3/src/components/homepage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class HomePage extends React.Component {
+  render(fname) {
+    return <h1>Hello, My name is:  {this.props.firstName}</h1>
+  }
+}
+
+export default HomePage;
