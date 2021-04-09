eab9138f300fab94822a019384211c4c435bc02b
ta396618 <59808989+ta396618@users.noreply.github.com>
2021-04-10 03:01:46 +0500

    lab 11

---
+++ Lab11/Database.js
@@ -0,0 +1,86 @@
+import MongoClient from 'mongodb';
+
+export default class Database {
+	constructor() {
+		this.url = 'mongodb+srv://TasfiaRaviba:Jg6VnuC7teDQtwLb@cluster0.yuzwq.mongodb.net';
+		this.connection = null;
+		this.db = null;
+		this.collection = null;
+	}
+
+	connect() {
+		MongoClient.connect(this.url, { useNewUrlParser: true })
+			.then((connection) => {
+				this.connection = connection;
+				this.db = this.connection.db('lab11');
+				this.collection = this.db.collection('books');
+				console.log('connected');
+			})
+			.catch((error) => {
+				console.log('error occured', error);
+			});
+	}
+	close() {
+		if (this.connection) {
+			this.connection.close();
+		}
+	}
+	async createOne(ISBN, title, author, description) {
+		try {
+			const result = await this.collection.insertOne({
+				ISBN: ISBN,
+				title: title,
+				author: author,
+				description: description,
+			});
+			//return created document
+			return result.ops[0];
+		} catch (error) {
+			console.log(error);
+		}
+	}
+	async readOne(ISBN) {
+		try {
+			const result = await this.collection.findOne({ ISBN: ISBN });
+			if (result) {
+				return result;
+			} else {
+				return 'not found';
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	}
+	async readMany(query) {
+		try {
+			const result = await this.collection.find(query).toArray();
+			console.log(result);
+			return result;
+		} catch (error) {
+			console.log(error);
+		}
+	}
+	async updateOne(ISBN, data) {
+		try {
+			const result = await this.collection.updateOne({ ISBN: ISBN }, { $set: data });
+			if (result.matchedCount == 0) {
+				return 'no document found';
+			} else if (result.modifiedCount == 0) {
+				return 'values not updated remain same as previous';
+			} else {
+				// return modified fields
+				return data;
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	}
+	async deleteOne(ISBN) {
+		try {
+			const result = await this.collection.deleteOne({ ISBN: ISBN });
+			return result.deletedCount;
+		} catch (error) {
+			console.log(error);
+		}
+	}
+}
---
+++ Lab11/Server.js
@@ -0,0 +1,71 @@
+import Express from 'express';
+import CORS from 'cors';
+import Database from './Database.js';
+
+const App = Express();
+const port = 45030;
+
+const database = new Database();
+database.connect();
+
+App.use(CORS());
+App.use(Express.json());
+
+App.put('/books/:ISBN', function (req, res) {
+	const ISBN = req.params.ISBN;
+	const title = req.body.title;
+	const author = req.body.author;
+	const description = req.body.description;
+	database
+		.createOne(ISBN, title, author, description)
+		.then((result) => {
+			res.json({
+				created_document: result,
+			});
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+});
+App.get('/books/:ISBN', function (req, res) {
+	const ISBN = req.params.ISBN;
+	database.readOne(ISBN).then((result) => {
+		res.json({
+			book: result,
+		});
+	});
+});
+App.post('/books/search', function (req, res) {
+	database
+		.readMany(req.query)
+		.then((result) => {
+			res.json({
+				books: result,
+			});
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+});
+App.patch('/books/:ISBN', function (req, res) {
+	const ISBN = req.params.ISBN;
+	database.updateOne(ISBN, req.body).then((result) => {
+		res.json({
+			updated_document: result,
+		});
+	});
+});
+App.delete('/books/:ISBN', function (req, res) {
+	const ISBN = req.params.ISBN;
+	database.deleteOne(ISBN).then((result) => {
+		// respond with delete count
+		res.json({
+			books: result,
+		});
+	});
+});
+App.listen(port, () => {
+	console.log(`listening on port ${port}`);
+});
+
+database.close();
